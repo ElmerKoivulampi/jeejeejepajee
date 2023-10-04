355571ae9449e32e0aaf7093ebefa41884096e44
Elmer Koivulampi <elmerkoivulampi@outlook.com>
2023-10-04 12:04:23 +0300

    HelloWorld.java: main(): Add main class

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+class main{
+	public static void main(String[] args) {
+		System.out.println("Hello" + name);
+
+	Random num = new Random();
+
+	int upperbound = 10;
+
+	if(num % 2==1) {
+		name = "Elmer";
+	} else {
+		name = "Pyry";
+}
+
